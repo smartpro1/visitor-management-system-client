13a84c75a520a4e983a185dadc57ee568a25c695
smartpro1 <akeni.promise@yahoo.com>
2020-06-04 23:32:53 +0100

    landing page bug fix

--- src/components/Layout/Landing.js
+++ src/components/Layout/Landing.js
@@ -12,7 +12,7 @@ class Landing extends Component {
         <h1 className="header-landing">
           Hi, I'm your efficient visitor management system
         </h1>
-        <p className="lead">
+        <p className="lead-landing">
           <mark>Register</mark> or <mark>Login</mark> to effectively manage your
           organization's visitors from
           <mark>start</mark> to <mark>finish</mark>
@@ -37,7 +37,7 @@ class Landing extends Component {
     if (isValidToken && adminCredentials) {
       signUp = (
         <div className="mt-5">
-          <h3 className="display-3 mt-4">
+          <h3 className="display-3 mt-4 header-landing">
             Hi, <em>{adminCredentials.fullname}</em>, hope your day has been
             good so far!{" "}
             <Link to="/dashboard" className="btn  btn-primary">
